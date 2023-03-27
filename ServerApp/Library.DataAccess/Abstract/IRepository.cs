54ca64e09fff6ed2d5da00a2207031f789239c7e
Alperen Polat <105422632+p0rt1a@users.noreply.github.com>
2023-03-28 00:35:15 +0300

    Backend init

---
+++ ServerApp/Library.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.DataAccess.Abstract
+{
+    public interface IRepository<T>
+        where T : new()
+    {
+        ICollection<T> Get();
+        T Get(int id);
+        T Create(T entity);
+        T Update(T entity);
+        void Delete(int id);
+    }
+}
